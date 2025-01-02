fc80a4b14e85e02d1df84aecbc5ff1b36266d029
kishanth <kishanth@LAPTOP-AGR3S4NJ>
2025-01-02 09:25:06 +0530

    first commit

---
+++ src/org/data/OddNumber.java
@@ -0,0 +1,13 @@
+package org.data;
+
+public class OddNumber {
+	public static void main(String[]args) {
+		for( int i=0;i<11;i++) {
+	    if (i%2==1) {
+	    	System.out.println(i);
+	    }
+		
+		}		
+		}
+}
+
